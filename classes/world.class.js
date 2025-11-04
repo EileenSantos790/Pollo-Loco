157afe95288bcd060791b7cdf58be5ea084e3b71
Eileen <eileen.santos@outlook.de>
2025-11-04 23:46:05 +0100

    Repositioning clouds

--- classes/world.class.js
+++ classes/world.class.js
@@ -161,6 +161,7 @@ class World {
         this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
         this.ctx.translate(-this.camera_x, 0);
         this.addToMap(this.statusBar);
         this.addToMap(this.coinStatusBar);
@@ -172,7 +173,6 @@ class World {
         this.addToMap(this.character);
         this.checkEndbossActivation();
         this.addObjectsToMap(this.level.enemies);
-        this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.collectableObjects);
         this.addObjectsToMap(this.throwableObjects);
 
